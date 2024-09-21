b9cafc3185b68d113bf1d97a54463d22ec41ab33
Oksana <git@github.com:OperaJAVA/Code-Like-a-Pro.git>
2024-09-21 14:58:40 +0300

    add MainActivity.kt

--- app/src/main/java/ru/netology/test/MainActivity.kt
+++ app/src/main/java/ru/netology/test/MainActivity.kt
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
 import ru.netology.test.databinding.ActivityMainBinding
-
 import ru.netology.test.viewmodel.PostViewModel
 
 class MainActivity : AppCompatActivity() {
@@ -14,50 +13,42 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         val viewModel by viewModels<PostViewModel>()
-           viewModel.data.observe(this){ post ->
+        viewModel.data.observe(this) { post ->
+            with(binding) {
+                author.text = post.author
+                published.text = post.published
+                textNetology.text = post.content
+                likeNumber.text = formatNumber(post.liked)
+                shareNumber.text = formatNumber(post.repost)
 
+                favoriteNumber.text = formatNumber(post.liked)
+                favorite.setImageResource(
+                    if (post.likedByMe) R.drawable.ic_baseline_favorite_border_red
+                    else R.drawable.ic_baseline_favorite_border_24
+                )
 
+                favorite.setOnClickListener {
+                    viewModel.favorite() // Обработка лайка теперь в ViewModel
+                }
 
-               with(binding){
-                author.text=post.author
-                published.text=post.published
-                textNetology.text=post.content
-                likeNumber.text=post.liked.toString()
-                shareNumber.text=post.repostByMe.toString()
+                share.setImageResource(
+                    if (post.repostByMe) R.drawable.ic_baseline_share_24 //
+                    else R.drawable.ic_baseline_share
+                )
 
-
-
-
-                   favoriteNumber.text = formatNumber(post.liked)
-                   if (post.likedByMe){
-                       favorite.setImageResource(if(post.likedByMe)R.drawable.ic_baseline_favorite_border_red else R.drawable.ic_baseline_favorite_border_24)
-
-                   }
-                   favorite.setOnClickListener {
-                       post.likedByMe = !post.likedByMe
-                       if (post.likedByMe) post.liked++ else post.liked--
-                       favorite.setImageResource(if(post.likedByMe)R.drawable.ic_baseline_favorite_border_red else R.drawable.ic_baseline_favorite_border_24)
-                       favoriteNumber.text = formatNumber(post.liked)
-                   }
-
-                   shareNumber.text= post.repost.toString()
-                   if (post.repostByMe){
-                       share.setImageResource(R.drawable.ic_baseline_favorite_border_24)
-                   }
-                   share.setOnClickListener {
-                       post.repostByMe = !post.repostByMe
-                       post.repost++
-                       shareNumber.text= post.repost.toString()
-                   }
+                share.setOnClickListener {
+                    viewModel.share() // Обработка репоста теперь в ViewModel
+                }
             }
-           }
-        binding.favorite.setOnClickListener{
-            viewModel.favorite()
         }
+
         println("onCreate $this")
     }
-    override fun onStart(){
+
+    // Остальные жизненные циклы активности остаются без изменений
+    override fun onStart() {
         super.onStart()
         println("onStart $this")
     }
@@ -81,5 +72,4 @@ class MainActivity : AppCompatActivity() {
         super.onDestroy()
         println("onDestroy $this")
     }
-
 }
